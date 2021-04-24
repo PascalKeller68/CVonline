52f6e6e2dfccdc661cec7bfa9091125074d18b62
PascalKeller68 <pascalkeller99@gmail.com>
2021-04-24 13:06:03 +0200

    mise a jour mineur

--- migrations/Version20210419075301.php
+++ migrations/Version20210419075301.php
@@ -24,7 +24,7 @@ final class Version20210419075301 extends AbstractMigration
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, name VARCHAR(50) NOT NULL, last_name VARCHAR(50) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE project ADD CONSTRAINT FK_2FB3D0EEBE15C1E4 FOREIGN KEY (relation_user_id) REFERENCES user (id)');
         // $this->addSql('INSERT INTO user (email, roles, password, name, last_name) 
-        // VALUES ("pascalkeller99@gmail.com", "{"ROLE_ADMIN"}","$2y$12$gPiPxVqblvxBwUkDve.D4ekuFVDwhMxeOPCubxr/NNwGzDeoNL6lq", "Keller", "pascal")');
+        // VALUES ("pascalkeller99@gmail.com", ["ROLE_ADMIN"],"$2y$12$gPiPxVqblvxBwUkDve.D4ekuFVDwhMxeOPCubxr/NNwGzDeoNL6lq", "Keller", "pascal")');
     }
 
     public function down(Schema $schema): void
